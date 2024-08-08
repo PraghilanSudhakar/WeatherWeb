c1c52852deeb35672a8bc6095adf3fca6c9d3014
PragTheDev <73265950+PraghilanSudhakar@users.noreply.github.com>
2024-08-07 22:04:48 -0500

    Update Humidity.jsx

--- components/widgets/Humidity.jsx
+++ components/widgets/Humidity.jsx
@@ -6,7 +6,7 @@ export const Humidity = (props) => {
     <Card className="rounded-lg shadow-md bg-white dark:bg-gray-800 flex flex-col items-center justify-center">
       <CardHeader className="px-5 py-3 border-b border-gray-200 dark:border-gray-600">
         <CardTitle className="text-3xl font-semibold text-gray-900 dark:text-gray-100 ">
-          Pressure
+          Humidity
         </CardTitle>
       </CardHeader>
       <CardContent className="p-5 w-full flex flex-col items-center justify-center">
